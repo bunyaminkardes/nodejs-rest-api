4934f24baefa3edfe83aacb0b2a0f918abee5617
Bünyamin Kardeş <bunyaminkardes@outlook.com>
2023-05-05 05:27:59 +0300

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const usersController = {};
-const userModel = require('../models/userModel.js'); //ilgili model'ı controller'a dahil et.
+const mongoose = require("mongoose");
+const User = require('../models/userModel.js'); //ilgili model'ı controller'a dahil et.
 
 usersController.getAllUsers = (req, res) => {
     res.send('oldu canım');
@@ -11,8 +12,7 @@ usersController.getUser = (req, res) => {
 }
 
 usersController.createUser = async (req, res) => {
-    const user = req.body;
-    const post = new userModel({
+    const post = new User({
         username : req.body.username,
         password : req.body.password,
         email : req.body.email
@@ -26,14 +26,29 @@ usersController.createUser = async (req, res) => {
     }
 }
 
-usersController.updateUser = (req, res) => {
-    const userID = req.params.id;
-    res.send('güncellenecek olan kullanıcı : ' + userID);
+usersController.updateUser = async (req, res) => { //?
+    const id = req.params.id;
+    const {username, password, email} = req.body;
+    const updatedUser = {_id: id, username, password, email};
+
+    await User.findByIdAndUpdate(id, updatedUser);
+
+    res.json(updatedUser);
 }
 
-usersController.deleteUser = (req, res) => {
-    const userID = req.params.id;
-    res.send('silinecek olan kullanıcı : ' + userID); 
+usersController.deleteUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({message : "Bu id değerine sahip bir kullanıcı bulunamadı."});
+        }
+        await User.findByIdAndDelete(id);
+        res.json({message : "kullanıcı başarıyla silindi."});
+    }
+    catch(error)
+    {
+        res.status(400).json({message : error.message});
+    } 
 }
 
 module.exports = usersController;
